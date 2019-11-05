b169f8400d0ca927c50edd527e55d7b317708bf9
toto13-git <56705907+toto13-git@users.noreply.github.com>
2019-11-05 10:29:30 +0900

    自動更新修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,29 @@
-$(function(){ 
-      function buildHTML(message){
-        image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >`: "";
-        var html =
-          `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-              ${image}
-            </div>
-           </div>`
-        return html;
-      } 
-      
+$(function(){
+
+  function buildHTML(message){
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >`: "";
+    
+    var html =`<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+    return html;
+  }
+
+
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -46,7 +49,7 @@ $('#new_message').on('submit', function(e){
     });
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data("message-id");
+      // var last_message_id = $('.message:last').data("message-id");
       $.ajax({
         url: "api/messages",
         type: 'GET',
@@ -55,6 +58,7 @@ $('#new_message').on('submit', function(e){
       })
  
       .done(function(messages) {
+
         var insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildHTML(message);
@@ -65,7 +69,7 @@ $('#new_message').on('submit', function(e){
       })
 
       .fail(function() {
-          alert('自動更新に失敗しました');
+        alert('自動更新に失敗しました');
       });
     }
   };
